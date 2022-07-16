35525f56b0fd6be1407320058a43901c3aac0ec8
Futlop <nickaposto2003@gmail.com>
2022-07-16 10:11:59 -0700

    Started Making Dice Holder

--- My project/Assets/Scripts/HolderScript.cs
+++ My project/Assets/Scripts/HolderScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class HolderScript : MonoBehaviour
 {
+    private ContactFilter2D Filter = new ContactFilter2D();
+    public Collider2D HolderCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Filter.NoFilter();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Collider2D[] Results = new Collider2D[0];
+        if(HolderCollider.OverlapCollider(Filter, Results) > 0)
+        {
+            for(int i = 0; i < Results.Length; i++)
+            {
+                if(Results[i].gameObject.tag == "Dice")
+                {
+
+                }
+            }
+        }
     }
 }
