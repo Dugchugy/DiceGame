5a06d53ed0a6e3ea0b93d9c54a90bf19be9a5b41
Futlop <nickaposto2003@gmail.com>
2022-07-16 12:28:09 -0700

    Dice and holders now in level select screen

--- My project/Assets/Scripts/HolderScript.cs
+++ My project/Assets/Scripts/HolderScript.cs
@@ -6,25 +6,37 @@ public class HolderScript : MonoBehaviour
 {
     private ContactFilter2D Filter = new ContactFilter2D();
     public Collider2D HolderCollider;
+    private GameObject[] Die;
+    public bool Snapped = false;
+    public int HolderNum = -1;
 
     // Start is called before the first frame update
     void Start()
     {
+        Die = GameObject.FindGameObjectsWithTag("Dice");
         Filter.NoFilter();
+        HolderCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Collider2D[] Results = new Collider2D[0];
-        if(HolderCollider.OverlapCollider(Filter, Results) > 0)
+        for(int i = 0; i < Die.Length; i++)
         {
-            for(int i = 0; i < Results.Length; i++)
+            Vector2 CP = HolderCollider.ClosestPoint(Die[i].transform.position);
+            if(Die[i].GetComponent<Collider2D>().OverlapPoint(CP) && Snapped == false)
             {
-                if(Results[i].gameObject.tag == "Dice")
-                {
-
-                }
+                Die[i].transform.position = transform.position;
+                Snapped = true;
+            }
+            if(!Die[i].GetComponent<Collider2D>().OverlapPoint(CP) && Snapped == true)
+            {
+                Snapped = false;
+                HolderNum = -1;
+            }
+            if(Die[i].GetComponent<Collider2D>().OverlapPoint(CP) && Snapped == true)
+            {
+                HolderNum = Die[i].GetComponent<DiceScript>().DiceNum;
             }
         }
     }
